6b72406c72d7d0dec0d2dd2577774c525b89a949
Marqzzs <maumarqz007@gmail.com>
2023-09-13 10:07:42 -0300

    a

---
+++ webapi.inlock.dbfirst.manha/Controllers/EstudioController.cs
@@ -0,0 +1,106 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.inlock.dbfirst.manha.Domains;
+using webapi.inlock.dbfirst.manha.Interfaces;
+using webapi.inlock.dbfirst.manha.Repository;
+
+namespace webapi.inlock.dbfirst.manha.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+
+    public class EstudioController : ControllerBase
+    {
+        private IEstudioRepository _estudioRepository { get; set; }
+
+        public EstudioController()
+        {
+            _estudioRepository = new EstudioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_estudioRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("ComJogos")]
+        public IActionResult GetWithGames() 
+        {
+            try
+            {
+                return Ok(_estudioRepository.ListarComJogos());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            try
+            {
+                return Ok(_estudioRepository.BuscarPorId(id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id) 
+        {
+            try
+            {
+                _estudioRepository.Deletar(id);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Estudio estudio)
+        {
+            try
+            {
+                _estudioRepository.Cadastrar(estudio);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Update(Guid id, Estudio estudio)
+        {
+            try
+            {
+                _estudioRepository.Atualizar(id, estudio);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
